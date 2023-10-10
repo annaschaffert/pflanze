b8dc7e1a87fc6f611a4599f55f720c7fd20a88a2
lynnbenz <lynn.benz@stud.fhgr.ch>
2023-10-10 11:29:00 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -10,30 +10,3 @@ const supa = supabase.createClient(supabaseUrl, supabaseKey, {
 });
 
 export { supa }
-
-// Funktion zum Hinzufügen eines neuen Benutzers zur Datenbank
-async function addUserToDatabase(first_name, last_name, email) {
-    try {
-      const { data, error } = await supabase.from('user').insert([
-        {
-          first_name,
-          last_name,
-          email,
-        },
-      ]);
-  
-      if (error) {
-        console.error('Fehler beim Hinzufügen des Benutzers:', error);
-        return false;
-      }
-  
-      console.log('Benutzer wurde erfolgreich hinzugefügt:', data);
-      return true;
-    } catch (error) {
-      console.error('Fehler beim Hinzufügen des Benutzers:', error);
-      return false;
-    }
-  }
-  
-  // Beispielaufruf der Funktion zum Hinzufügen eines Benutzers
-  addUserToDatabase('Max', 'Mustermann', 'max@example.com');
