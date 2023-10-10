f272a608fa99996090e65df0425e0a2626f54b13
lynnbenz <lynn.benz@stud.fhgr.ch>
2023-10-10 11:28:55 +0200

    Update registrierung.js

--- js/registrierung.js
+++ js/registrierung.js
@@ -1,19 +1,50 @@
+import { supa } from "../js/supabase.js";
+
 document.addEventListener("DOMContentLoaded", function () {
     const registrationForm = document.getElementById("registration-form");
   
     registrationForm.addEventListener("submit", function (event) {
       event.preventDefault();
   
-      const vorname = document.getElementById("first_name").value;
-      const nachname = document.getElementById("last_name").value;
+      const vorname = document.getElementById("vorname").value;
+      const nachname = document.getElementById("nachname").value;
       const email = document.getElementById("email").value;
   
       // Hier können Sie die eingegebenen Daten weiterverarbeiten, z.B. an einen Server senden.
-  
+      
+      // hinzufügen der daten mit supa
+        addUserToDatabase(vorname, nachname, email);
+
+        async function addUserToDatabase(first_name, last_name, email) {
+            try {
+              const { data, error } = await supa.from('User').insert([
+                {
+                  first_name,
+                  last_name,
+                  email,
+                },
+              ]);
+          
+              if (error) {
+                console.error('Fehler beim Hinzufügen des Benutzers:', error);
+                return false;
+              }
+          
+              console.log('Benutzer wurde erfolgreich hinzugefügt:', data);
+              return true;
+            } catch (error) {
+              console.error('Fehler beim Hinzufügen des Benutzers:', error);
+              return false;
+            }
+          }
+
+
       // Beispiel: Ausgabe der Daten in der Konsole
-      console.log("Vorname: " + first_name);
-      console.log("Nachname: " + last_name);
+      console.log("Vorname: " + vorname);
+      console.log("Nachname: " + nachname);
       console.log("E-Mail-Adresse: " + email);
+
+
   
       // Zurücksetzen des Formulars (optional)
       registrationForm.reset();
