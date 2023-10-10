de2e651d0f199f45f9a0a8e9b2dc1d0763505e01
lynnbenz <lynn.benz@stud.fhgr.ch>
2023-10-10 16:19:21 +0200

    Create pflanze-loeschen.js

---
+++ js/pflanze-loeschen.js
@@ -0,0 +1,27 @@
+import { supa } from "./supabase.js";
+
+// eventlistener on delete button
+document.getElementById('deletePlantButton').addEventListener('click', deletePlant);
+const result = document.getElementById('result');
+
+
+// Funktion zum Löschen eines Benutzers anhand seiner ID
+async function deletePlant() {
+  const id = document.getElementById('id').value;
+
+  try {
+    const { data, error } = await supa.from('Plant').delete().eq('id', id);
+
+    if (error) {
+      console.error('Fehler beim Löschen der Pflanze:', error);
+    } else {
+      console.log('Pflanze wurde erfolgreich gelöscht:', data);
+        result.innerHTML = "Pflanze wurde erfolgreich gelöscht";
+        
+    }
+  } catch (error) {
+    console.error('Fehler beim Löschen der Pflanze:', error);
+  }
+}
+
+//deletePlant();
