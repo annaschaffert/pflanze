45a4d1ecac95fc2e8967eb7b1c7b33d223b86932
annaschaffert <anna.schaffert@stud.fhgr.ch>
2023-11-06 16:34:59 +0100

    update

--- js/supabase.js
+++ js/supabase.js
@@ -4,9 +4,9 @@ console.log("Initialisierung Supabase");
 const supabaseUrl = 'https://ghuslyfkuvukzhimvdhf.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdodXNseWZrdXZ1a3poaW12ZGhmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTY4MzcyMzAsImV4cCI6MjAxMjQxMzIzMH0.hP8ekeTgxKjWGwHqZ0HkoopPEltoft238Bk0lMUBmss'
 const supa = supabase.createClient(supabaseUrl, supabaseKey, {
-    auth: {
-        redirectTo: window.location.origin,  // This will redirect back to the page where the request originated from
-    },
+  auth: {
+      redirectTo: 'https://423521-6.web.fhgr.ch/pflanze/',  // Hier die korrekte Weiterleitungs-URL eintragen
+  },
 });
 
 async function getSignedUrl(filePath) {
