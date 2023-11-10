1a478bcdf81a2de3dcde33388f46833aa7deb03d
annaschaffert <anna.schaffert@stud.fhgr.ch>
2023-11-10 13:44:09 +0100

    upload

--- js/magiclink.js
+++ js/magiclink.js
@@ -21,7 +21,7 @@ function updateUserStatus(user) {
         window.location.href = "/meine-pflanzen.html";
     } else {
         userStatusElement.textContent = "Not authenticated.";
-        window.location.href = "/index.html";
+        
     }
 }
 
